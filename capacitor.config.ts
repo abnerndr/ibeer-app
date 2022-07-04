93c91398f280c557310a8fd60fe1746947325974
Matheus Prado <matheusfp.dev@gmail.com>
2022-07-04 19:53:17 -0300

    update app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,37 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'astro-library',
-  webDir: 'public',
-  bundledWebRuntime: false
-};
+  appId: 'com.ibeer.app',
+  appName: 'ibeer',
+  webDir: 'out',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: true,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#999999',
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+      useDialog: true
+    },
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert']
+    }
+  },
+  ios: {
+    contentInset: 'always'
+  },
+  cordova: {},
+  server: {
+    url: 'https://ibeer-app-sepia.vercel.app'
+  }
+}
 
-export default config;
+export default config
